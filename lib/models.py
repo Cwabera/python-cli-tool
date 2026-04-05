8055be74ca3f672bcc7469c723edf287067f00bb
Charles <charlesngatia.cn@gmail.com>
2026-04-05 16:01:29 +0300

    Fiin task complete message

--- lib/models.py
+++ lib/models.py
@@ -8,38 +8,10 @@ class Task:
 
     def complete(self):
         self.completed = True
-        print(f"Task '{self.title}' marked as completed!")
+        print(f"✅ Task '{self.title}' completed.")
 
     def to_dict(self):
         return {
             "title": self.title,
             "completed": self.completed
-        }
-
-
-class User:
-    def __init__(self, name):
-        if not name or not name.strip():
-            raise ValueError("User name cannot be empty")
-
-        self.name = name.strip()
-        self.tasks = []
-
-    def add_task(self, task):
-        if not isinstance(task, Task):
-            raise ValueError("task must be a Task object")
-
-        self.tasks.append(task)
-        print(f"Task '{task.title}' added for user '{self.name}'.")
-
-    def get_task_by_title(self, title):
-        for task in self.tasks:
-            if task.title.lower() == title.lower():
-                return task
-        return None
-
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "tasks": [task.to_dict() for task in self.tasks]
         }
